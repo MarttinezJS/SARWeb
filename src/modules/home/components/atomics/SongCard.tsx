57bba7b3b46c6db014c5b31e474544c28e6491f9
Jhonatan Martinez <jhonatan.martinez@fsteam.co>
2024-09-17 11:38:07 -0500

    Variable de entorno de jwt

--- src/modules/home/components/atomics/SongCard.tsx
+++ src/modules/home/components/atomics/SongCard.tsx
@@ -1,4 +1,15 @@
-import { Button, Card, Image, Spacer, Tooltip } from "@nextui-org/react";
+import {
+  Button,
+  Card,
+  Image,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalHeader,
+  Spacer,
+  Tooltip,
+  useDisclosure,
+} from "@nextui-org/react";
 import { Song } from "../../models";
 import { IoMdListBox } from "react-icons/io";
 
@@ -8,8 +19,27 @@ interface SongCardProps {
 }
 
 export const SongCard = ({ song, duration }: SongCardProps) => {
+  const { onOpen, onOpenChange, isOpen } = useDisclosure();
   return (
     <div className="p-1">
+      <Modal
+        scrollBehavior="inside"
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+      >
+        <ModalContent>
+          {() => (
+            <>
+              <ModalHeader>
+                <p>{song.title}</p>
+              </ModalHeader>
+              <ModalBody>
+                <pre className="font-sans">{song.lyrics}</pre>
+              </ModalBody>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
       <Card fullWidth shadow="none" className="bg-yellow-background">
         <div className="flex justify-between">
           <div className="flex">
@@ -28,7 +58,12 @@ export const SongCard = ({ song, duration }: SongCardProps) => {
           </div>
           {song.lyrics && (
             <Tooltip content="Letra.">
-              <Button isIconOnly variant="light" color="primary">
+              <Button
+                onPress={onOpen}
+                isIconOnly
+                variant="light"
+                color="primary"
+              >
                 <IoMdListBox className="text-secondary" />
               </Button>
             </Tooltip>
