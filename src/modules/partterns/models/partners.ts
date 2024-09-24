4585e45d55d1f90a5101061c21ae7adbf9574020
Jhonatan Martinez <jhonatan.martinez@fsteam.co>
2024-09-23 21:52:56 -0500

    Tabla de patrocinadores

---
+++ src/modules/partterns/models/partners.ts
@@ -0,0 +1,13 @@
+export interface Partner {
+  id: string;
+  imageUrl: string;
+  active: boolean;
+  amount: number;
+  subscriptionDate: Date;
+  expirationDate: Date;
+  companyName: string;
+  contact: string;
+  nit: string;
+  email: string;
+  contactNumber: string;
+}
