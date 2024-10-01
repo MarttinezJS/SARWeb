064a7ec2d203ea121cd424baf447c40363438f99
Jhonatan Martinez <jhonatan.martinez@fsteam.co>
2024-09-30 22:24:22 -0500

    Limpieza de codigo

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,6 @@ export const App = () => {
     !isAuthenticated && !(await sessionValid()) ? redirect("/") : null;
   useEffect(() => {
     sessionValid();
-    console.log(isAuthenticated);
   }, []);
 
   return (
