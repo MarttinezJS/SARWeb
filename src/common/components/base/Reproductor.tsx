a98fd6e403ad73f39b234e296afb9739be052f0d
Jhonatan Martinez <jhonatan.martinez@fsteam.co>
2024-10-28 14:01:35 -0500

    Cambio de botones en el reproductor

--- src/common/components/base/Reproductor.tsx
+++ src/common/components/base/Reproductor.tsx
@@ -1,8 +1,8 @@
-import { Button, Card, CardBody, Image } from "@nextui-org/react";
+import { Button, Card, CardBody, Image, Spacer } from "@nextui-org/react";
 import { useRef, useState } from "react";
 import ReactAudioPlayer from "react-audio-player";
-import { FaMusic } from "react-icons/fa";
-import { FaPause, FaPlay } from "react-icons/fa6";
+import { FaMusic, FaPlayCircle } from "react-icons/fa";
+import { FaCirclePause } from "react-icons/fa6";
 import { SlArrowRight } from "react-icons/sl";
 import { PlayerProgress } from "../atomics";
 
@@ -22,7 +22,7 @@ export const Reproductor = () => {
         isIconOnly
         variant="solid"
         color="secondary"
-        className={`absolute z-10 top-1/2 ${
+        className={`absolute z-10 top-[4.5rem] ${
           expanded ? "-left-5" : "-left-10"
         } duration-300`}
         onPress={() => {
@@ -33,70 +33,63 @@ export const Reproductor = () => {
       </Button>
       <Card
         isBlurred
-        className=" max-w-[610px] border-none bg-background/60 dark:bg-default-100/50"
+        className=" max-w-[650px] border-none bg-background/60 dark:bg-default-100/50"
         shadow="sm"
         fullWidth
       >
         <CardBody>
           <div className="grid grid-cols-12 gap-6 md:gap-4 items-center justify-center">
-            <div className="relative col-span-6 md:col-span-4">
-              <Image
-                alt="Album cover"
-                className="object-cover"
-                height={200}
-                shadow="md"
-                src={"/logo.svg"}
-              />
+            <div className="relative flex col-span-6 h-full w-auto md:col-span-4 items-center justify-center">
+              <Button
+                isIconOnly
+                radius="full"
+                variant="shadow"
+                color="primary"
+                className="w-max h-max"
+                onPress={() => {
+                  setPlaying(!playing);
+                  if (playing) {
+                    player.current?.audioEl.current?.pause();
+                  } else {
+                    player.current?.audioEl.current?.play();
+                    setExpanded(false);
+                  }
+                }}
+              >
+                <div className="text-7xl m-5 flex justify-center items-center">
+                  {playing ? <FaCirclePause /> : <FaPlayCircle />}
+                </div>
+              </Button>
             </div>
 
             <div className="flex flex-col col-span-6 md:col-span-8">
-              <div className="flex justify-between items-start">
-                <div className="flex flex-col gap-0">
-                  {/* <h3 className="font-semibold text-foreground/90">
-                    Daily Mix
-                  </h3>
-                  <p className="text-small text-foreground/80">12 Tracks</p> */}
-                  <h1 className="text-large font-medium mt-2">
-                    Sigue adelante radio
-                  </h1>
-                </div>
-                {/* <Button
-                isIconOnly
-                className="text-default-900/60 data-[hover]:bg-foreground/10 -translate-y-2 translate-x-2"
-                radius="full"
-                variant="light"
-                onPress={() => setLiked((v) => !v)}
-              >
-                <HeartIcon
-                  className={liked ? "[&>path]:stroke-transparent" : ""}
-                  fill={liked ? "currentColor" : "none"}
+              <div className="flex">
+                <Image
+                  alt="Album cover"
+                  className="object-cover"
+                  height={100}
+                  shadow="md"
+                  src={"/logo.svg"}
                 />
-              </Button> */}
+                <Spacer />
+                <div className="flex justify-between items-end">
+                  <div className="flex flex-col gap-0">
+                    {/* <h3 className="font-semibold text-foreground/90">
+                      Daily Mix
+                    </h3>
+                    <p className="text-small text-foreground/80">12 Tracks</p> */}
+                    <h1 className="text-large font-medium mt-2 w-max">
+                      Sigue adelante radio
+                    </h1>
+                  </div>
+                </div>
               </div>
               <PlayerProgress player={player.current?.audioEl.current} />
               <ReactAudioPlayer
                 src="https://server2.ejeserver.com:8826/stream"
                 ref={player}
               />
-              <div className="flex w-full items-center justify-center">
-                <Button
-                  isIconOnly
-                  radius="full"
-                  size="lg"
-                  variant="light"
-                  onPress={() => {
-                    setPlaying(!playing);
-                    if (playing) {
-                      player.current?.audioEl.current?.pause();
-                    } else {
-                      player.current?.audioEl.current?.play();
-                      setExpanded(false);
-                    }
-                  }}
-                >
-                  {playing ? <FaPause /> : <FaPlay />}
-                </Button>
-              </div>
+              <div className="flex w-full items-center justify-center"></div>
             </div>
           </div>
         </CardBody>
